b044d1b1ec74a6108d824ea5269e4e731335ef0b
aliskhannn <aliskhan2021@mail.ru>
2025-01-27 20:36:18 +0300

    Added file creation functionality

--- main.go
+++ main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
+	"strings"
 )
 
 func main() {
@@ -40,9 +43,29 @@ func main() {
 	}
 }
 
+func printMenu(actions map[int]string) {
+	fmt.Print("\nChoose an action:\n\n")
+	for k, v := range actions {
+		fmt.Printf("%d. %s\n", k, v)
+	}
+}
+
 func handleCreateFile() {
-	fmt.Println("Creating a file...")
-	// Реализация добавится позже
+	fileName := getFileNameFromUser("\nEnter file name (example.txt):")
+	if fileName == "" {
+		return
+	}
+
+	fmt.Println("\nEnter text for the file:")
+	text := getTextFromUser()
+
+	err := createFile(fileName, text)
+
+	if err != nil {
+		fmt.Println("Error writing to file:", err)
+	} else {
+		fmt.Printf("File %s successfully created!\n", fileName)
+	}
 }
 
 func handleEncryptFile() {
@@ -55,9 +78,37 @@ func handleDecryptFile() {
 	// Реализация добавится позже
 }
 
-func printMenu(actions map[int]string) {
-	fmt.Print("\nChoose an action:\n\n")
-	for k, v := range actions {
-		fmt.Printf("%d. %s\n", k, v)
+func getFileNameFromUser(prompt string) string {
+	var fileName string
+
+	fmt.Println(prompt)
+	_, err := fmt.Scan(&fileName)
+
+	if err != nil || !strings.HasSuffix(fileName, ".txt") {
+		fmt.Println("Invalid file name. Please try again.")
+		return ""
 	}
+
+	return fileName
+}
+
+func getTextFromUser() string {
+	reader := bufio.NewReader(os.Stdin)
+
+	text, err := reader.ReadString('\n')
+	if err != nil {
+		fmt.Println("Invalid input. Please try again.")
+		return ""
+	}
+
+	return text
+}
+
+func createFile(fileName, text string) error {
+	err := os.WriteFile(fileName, []byte(text), 0644)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
